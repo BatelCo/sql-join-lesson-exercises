6654eb77d0c07194b9c45ce60fb62f6841eda806
batelco <batel.cohen100@gmail.com>
2022-10-22 22:45:13 +0300

    spot check 4

--- solutions/check_4.sql
+++ solutions/check_4.sql
@@ -1,1 +1,21 @@
 USE sql_intro;
+
+-- SELECT s_name
+-- FROM 
+--     student_teacher AS st_t, -- also aliasing
+--     student AS st, 
+--     teacher AS te
+-- WHERE 
+--     te.t_name = "Foster" AND
+--     te.t_id = st_t.teacher_id AND
+--     st.s_id = st_t.student_id;
+
+SELECT s_name 
+FROM 
+    student AS s, 
+    teacher AS t, 
+    student_teacher AS st
+WHERE 
+    t.t_name = "Foster" AND
+    s.s_id = st.student_id AND
+    t.t_id = st.teacher_id;
